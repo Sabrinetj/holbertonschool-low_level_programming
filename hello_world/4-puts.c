6a4b4b6eb09000a60c1192d81e355846f23717a8
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-10-12 04:53:42 -0700

    puts prinf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #!/bin/bash
-include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Print a message beging with a specific char
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
