1f671ca47f8aea733d5f4e7e804c4ca2d9ab680e
root <zaghouanisabrine1030@gmail.com>
2023-10-17 06:23:39 -0700

    Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 /**
- * main - Display all single numberswith some condition
+ * main - Dispaly all letter of 16 base
  *
  * Return: 0
  */
 int main(void)
 {
 	int i;
+	char c;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
