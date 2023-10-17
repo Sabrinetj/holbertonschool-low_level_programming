85f34c051207c1502431ce819d6392131adac9a4
root <zaghouanisabrine1030@gmail.com>
2023-10-17 05:57:15 -0700

    alphabet in lowercase

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints all alphabet expt e and q
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
