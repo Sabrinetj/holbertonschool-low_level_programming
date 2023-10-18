40e859e4b20eabc519a639ca3f8e4dbf5a184510
root <zaghouanisabrine1030@gmail.com>
2023-10-18 07:35:27 -0700

    fi condition

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function that return the absulute value of int
+ * @n: the numebr to treat
+ *
+ * Return: the absulute number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
