954bc2edc35da88026786438ae985ce6034b19e3
root <zaghouanisabrine1030@gmail.com>
2023-10-18 04:52:08 -0700

    creat mainc

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - print_alphabet(void)
+ *
+ * Return: 0.
+ */
+ void print_aplphabet (void);
+{
+	char alpha;
+	for (aplha =0 ; a=<z; a++)
+		{
+	_putchar(c);
+}
+_putchar('\n');
+}
+
+
+
+	 
+		
