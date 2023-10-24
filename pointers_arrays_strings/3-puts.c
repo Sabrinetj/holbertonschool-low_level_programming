c094d1591f61eb20d7efed74775689e8767160aa
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-10-24 05:37:59 -0700

    imprimer caracter

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,15 +2,17 @@
 /**
  * _puts - Prints a string followed by a new line to stdout
  * @str: A pointer to the string to be printed
- *
+ * Return: void
  * Description: This function prints the provided string to stdout,
- * followed by a new line character.
+ * followed by a new line character
  */
 void _puts(char *str)
 {
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        putchar(str[i]);
-    }
-    putchar('\n');
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
