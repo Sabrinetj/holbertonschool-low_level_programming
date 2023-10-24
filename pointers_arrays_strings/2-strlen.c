249cc46a08e90fc11fe60554a595a31496be9a48
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-10-24 04:20:05 -0700

    lenght of string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * strlen char - the function that returns the length of a string
+ *@S: pointer caracter
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		lenght++;
+	}
+	return (lenght);
+}
+
