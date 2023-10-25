a6240990c5d2bdb29be349dda773f3a2f1561187
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-10-25 10:37:09 -0700

    print arry string

---
+++ pointers_arrays_strings/8-print_arry.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - function taht prints the countent ofthe array of int
+ * @a: the array
+ * @n: the array lenght
+ * Return: Always
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[n - 1]);
+		}
+	}
+	printf("\n");
+}
