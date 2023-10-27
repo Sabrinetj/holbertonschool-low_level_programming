09e193c35c2d198b7373391be52163d6c4cfd1cd
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-10-27 07:33:06 -0700

    unction that locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Function that shearch a charcter in string
+ * @s: the string to search in
+ * @c: the charcter to search
+ * Return: a sting that begin with a
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
