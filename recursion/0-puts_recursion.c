dcd07bddd0cd3b72971dcfc5ff7a2fcacf51dc0c
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-11-01 05:06:46 -0700

    pt manh manc

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include  "main.h"
+/**
+* _puts_recursion - fonction print a string
+* followed by a new line
+* @s : print a string
+* Retour : nothing
+*/
+void  _puts_recursion ( char  * s )
+{
+	if ( * s  ==  '\0' )
+	{
+		_putchar ( '\n' );
+		retour ;
+	}
+	_putchar ( * s );
+	s ++ ;
+	_puts_recursion ( s );
+}
