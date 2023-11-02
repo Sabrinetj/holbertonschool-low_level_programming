26112a086f8e5efa0f376a99983033c27e126f04
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-11-02 07:09:29 -0700

    arrgumrnt boucle

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to multiplay two number argument
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int v1 = 0, v2 = 0;
+
+	if (argc == 3)
+	{
+		v1 = atoi(argv[1]);
+		v2 = atoi(argv[2]);
+		printf("%d\n", v1 * v2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
