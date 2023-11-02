8a648852c4ce9903f3e03d9fabc65824d401645c
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-11-02 03:08:08 -0700

    strlen calculate

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+/**
+* _strlen_recursion - fonction returns the length of a string
+* @s : strlen recursion
+* Retour : similar function
+*/
+int  _strlen_recursion ( char  * s )
+{
+	if ( * s  ==  '\0' )
+		return ( 0 );
+	s ++ ;
+	return ( _strlen_recursion ( s ) +  1 );
+}
