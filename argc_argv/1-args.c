49d14f4ac79d1d7364e3a487477d47c362e82492
Sabrinetj <zaghouanisabrine1030@gmail.com>
2023-11-02 07:01:13 -0700

    arrgumentation

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of argument passed
+ * @argv: array pointing to arguement
+ * Return: 0 (Success)
+ */
+int main(int argc,  __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
