8760d87461212ea2faa3faef06d40eaaff6727b7
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-02-21 08:50:28 -0800

    number 0-9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Display all single numberswith some condition
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
