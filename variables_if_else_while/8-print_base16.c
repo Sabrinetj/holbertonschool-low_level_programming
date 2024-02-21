a50878ae230ca2adf383afdf1b3f538c3fb684c5
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-02-21 08:53:34 -0800

    boucle

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Dispaly all letter of 16 base
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
