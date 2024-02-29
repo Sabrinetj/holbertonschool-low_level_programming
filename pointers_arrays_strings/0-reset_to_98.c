f6597f49f9df6b25534709a580bb4004194c95c3
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-02-29 13:26:49 +0000

    pointeur arr

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that return all int value to 98
+ * @n: the entring number
+ * Return: an int that must have the value of 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
