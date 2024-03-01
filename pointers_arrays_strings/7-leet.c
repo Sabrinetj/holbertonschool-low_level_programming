a94a24c4472e8d491e8d957c1f1fe876aaf66844
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-03-01 11:40:51 +0000

    char leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,27 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 /**
-  * leet - Encodes a string into 1337
-  * @s: The string to encode
-  *
-  * Return: The encoded string
-  */
-char *leet(char *s)
+ * leet - a function that encodes a string into 1337
+ * @str: pointer to character
+ * Return: pointer to char
+ */
+char *leet(char *str)
 {
 	int i, j;
-	char letre[5] = {'A', 'E', 'O', 'T', 'L'};
-	char chifre[5] = {'4', '3', '0', '7', '1'};
-	
-	for (i = 0; i <= sizeof(*s); i++)
+	char a1[] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char a2[] = {'A', 'E', 'O', 'T', 'L', '\0'};
+	char a3[] = {'4', '3', '0', '7', '1', '\0'};
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 5; j++)
+		for (j = 0; a1[j] != '\0' && a2[j] != '\0' && a3[j] != '\0'; j++)
 		{
-		if (s[i] == letre[j] || s[i] - 32 == letre[j])
-		{
-			s[i] = chifre[j];
-		}
+			if ((str[i] == a1[j]) || (str[i] == a2[j]))
+			{
+				str[i] = a3[j];
+			}
 		}
 	}
-	return (s);
-
+	return (str);
 }
