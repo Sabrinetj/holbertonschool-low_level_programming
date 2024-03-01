a88308e3e0f278c78ec9e088da9fc1319127eb5c
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-03-01 11:28:15 +0000

    tab str mp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,32 @@
 #include "main.h"
 /**
- * _strcmp - a function that compares two strings
- * @s1: pointer to character
- * @s2: pointer to character
- * Return: integer
+ * _strcmp - function that compares two strings
+ * @s1: the first string to compare
+ * @s2: the secound string
+ * Return: +1 if is positive and -1 if is negative
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0;
 
-	for (i =  0; s1[i] != '\0'; i++)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			if (s1[i] == s2[j])
-				return (0);
-			else
-				return (s1[i] - s2[j]);
-		}
+		if (s1[i] != s2[i])
+	{
+		return (s1[i] - s2[i]);
 	}
-	return (0);
+	i++;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+	else if (s1[i] == '\0')
+	{
+		return (-s2[i]);
+	}
+	else
+	{
+		return (s1[i]);
+	}
 }
