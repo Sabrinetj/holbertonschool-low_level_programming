0f1575cf594cdef5e65b65090f393a202ea363cb
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-03-07 13:32:58 +0100

    prints name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of argument passed
+ * @argv: array pointing to arguements
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+		printf("%s\n", argv[argc]);
+	return (0);
+}
