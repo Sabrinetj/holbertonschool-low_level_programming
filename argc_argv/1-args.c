26c809d75773567cc2b1f7bd67ab03bdeac0b099
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-03-07 13:34:08 +0100

    prints number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: int
+*/
+
+int main(int argc,  __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
