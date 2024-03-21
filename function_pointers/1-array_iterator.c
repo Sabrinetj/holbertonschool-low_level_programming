17a045e4aa15c5a8ddf2bdd1f7b6f2eb0c810752
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-03-21 13:26:06 +0100

    arrey p

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - fun that execute a func given as para
+ *@array: parameter of the func
+ *@size: size of the arr to iterate
+ *@action: pointer to a func
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != 0 && action != 0)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
+
+
