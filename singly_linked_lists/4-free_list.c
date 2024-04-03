ef048308b593e98c8f74b62b3bade2868a79d10d
Sabrinetj <zaghouanisabrine1030@gmail.com>
2024-04-03 14:19:13 +0200

    head tmp

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list
+ * @head: The pointer to the first node of linked list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
